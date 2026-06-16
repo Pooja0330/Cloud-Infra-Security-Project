43864e90aee7982cec66227f817f54aa421e4e88
Pooja M <poojamahant0330@gmail.com>
2026-06-16 12:47:04 +0100

    Update vpc_setup_commands.sh

--- Step2-VPC-Architecture/vpc_setup_commands.sh
+++ Step2-VPC-Architecture/vpc_setup_commands.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# Create VPC with CIDR block 10.0.0.0/16
+aws ec2 create-vpc --cidr-block 10.0.0.0/16
 
+# Additional commands here for subnets, route tables, IGW, etc.
+# Use comments liberally to explain each step
