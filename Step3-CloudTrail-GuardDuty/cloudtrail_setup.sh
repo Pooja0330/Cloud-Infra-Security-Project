dd3750034b9b0300dc614e1d4a7ddf684b9a4328
Pooja M <poojamahant0330@gmail.com>
2026-06-16 12:48:24 +0100

    Update cloudtrail_setup.sh

--- Step3-CloudTrail-GuardDuty/cloudtrail_setup.sh
+++ Step3-CloudTrail-GuardDuty/cloudtrail_setup.sh
@@ -1,1 +1,4 @@
-# Script to enable CloudTrail  
+#!/bin/bash
+# Enable CloudTrail in all regions and deliver logs to S3 bucket
+aws cloudtrail create-trail --name MyTrail --s3-bucket-name my-cloudtrail-logs
+aws cloudtrail start-logging --name MyTrail
