084a458298ca895c9d50bec4b74a549d251a0c99
Pooja M <poojamahant0330@gmail.com>
2026-06-16 12:50:34 +0100

    Update aws_config_setup.sh

--- Step5-Compliance-Automation/aws_config_setup.sh
+++ Step5-Compliance-Automation/aws_config_setup.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+# Enable AWS Config with default rules
+aws configservice put-configuration-recorder --configuration-recorder name=default,roleARN=arn:aws:iam::<account-id>:role/config-role
+aws configservice start-configuration-recorder --configuration-recorder-name default
